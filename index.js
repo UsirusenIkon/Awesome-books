fa50226a04fcfd9b5e77dafb6a7323948ee68c1d
juharyimami <juharyimami2011@gmail.com>
2022-09-19 16:33:42 +0300

    fixig dynamic content

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const title=document.querySelector(".title");
+const author=document.querySelector(".author");
+const form=document.querySelector(".form");
+const submit=document.querySelector(".submit");
 let books = [
     book1 = 
     {title: 'title1',
@@ -15,19 +19,36 @@ let books = [
 
 let bookList = document.querySelector('.bookList');
 
-books.forEach(book => {
-    let title = document.createElement('h3');
-    title.innerText = book.title;
+function displayBook(){
+    bookList.innerText="";
+    books.forEach(book => {
+        let title = document.createElement('h3');
+        title.innerText = book.title;
+    
+        let author = document.createElement('h4');
+        author.innerText = book.author;
+    
+        let removeBtn = document.createElement('button');
+        removeBtn.innerText = 'Remove';
+    
+        let horizontalLine = document.createElement('hr');
+    
+        bookList.append(title, author, removeBtn, horizontalLine);
+    
+    });
+}
 
-    let author = document.createElement('h4');
-    author.innerText = book.author;
 
-    let removeBtn = document.createElement('button');
-    removeBtn.innerText = 'Remove';
-
-    let horizontalLine = document.createElement('hr');
-
-    bookList.append(title, author, removeBtn, horizontalLine);
-
+submit.addEventListener('click', addBooks=>{
+ let title1 = title.value;
+ let author2=author.value;
+ 
+ console.log(title1);
+ console.log(author2);
+ books.push(book={
+    title:title1,
+    author:author2
 });
-
+displayBook();
+console.log(books);
+});
